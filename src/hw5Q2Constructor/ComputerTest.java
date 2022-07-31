944f8da89784a719a1322bf0e894d31b1638b0b1
mishu2256 <mdkhalequzzman@mds-MacBook-Pro.local>
2022-07-31 19:56:58 -0400

    hw5

---
+++ src/hw5Q2Constructor/ComputerTest.java
@@ -0,0 +1,13 @@
+package hw5Q2Constructor;
+
+public class ComputerTest {
+
+	public static void method(String[] args) {
+		Computer computer = new Computer();
+
+		Computer computer1 = new Computer("Apple", "MacBook Air", "MacOS Mojave", 800, 'A', false);
+		
+		Computer computer2 = new Computer("HP", "windows", "pavilion", 600, 'A',false);
+
+	}
+}
---
+++ src/hw5Q3Constructor/StudentTest.java
@@ -0,0 +1,10 @@
+package hw5Q3Constructor;
+
+public class StudentTest {
+	public static void main(String[] args) {
+		Student student = new Student();
+		Student student1 = new Student("Md Khalequzzman", 223121, 'M', true, 3.33f);
+
+	}
+
+}
