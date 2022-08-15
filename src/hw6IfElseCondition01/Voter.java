ab189e5aff522c27e1562727f3aee06b1a6b64a2
mishu2256 <mdkhalequzzman@mds-MacBook-Pro.local>
2022-08-15 17:32:09 -0400

    hw

--- src/hw6IfElseCondition01/Voter.java
+++ src/hw6IfElseCondition01/Voter.java
@@ -2,22 +2,16 @@ package hw6IfElseCondition01;
 
 public class Voter {
 	public static void main(String[] args) {
-		System.out.println("Please add valid age");
-
-		int age = 31;
-
+		//System.out.println("Please add valid age");
+		int age = 17;
 		if (age == 18) {
 			System.out.println("I am a voter");
 		} else if (age < 18) {
 			System.out.println("I am not a voter");
-
 		} else if (age > 18) {
 			System.out.println("I am a voter");
-
 		} else {
 			System.out.println("Please add a valid age");
 		}
-
 	}
-
 }
